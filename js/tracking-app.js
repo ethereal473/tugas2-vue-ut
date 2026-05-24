dc89dcf5f959f74cd7f70cdb0c81bd4f92854240
ethereal473 <trikay07@gmail.com>
2026-05-24 20:56:47 +0700

    Tambah trackinglist

--- js/tracking-app.js
+++ js/tracking-app.js
@@ -14,18 +14,31 @@ var trackingApp = new Vue({
         { kode: "PAKET-UT-002", nama: "PAKET IPA Dasar", isi: ["BIOL4201","FISIP4001"], harga: 140000 }
       ],
 
-    trackingList: [],
+    trackingList: [
+      {
+        nomor: "DO2025-0001",
+        nim: "123456789",
+        nama: "Rina Wulandari",
+        status: "Dalam Perjalanan",
+        ekspedisi: "JNE",
+        tanggalKirim: "2025-08-25",
+        paket: "PAKET-UT-001",
+        total: 120000,
+        perjalanan: [
+          { waktu: "2025-08-25 10:12:20", keterangan: "Penerimaan di Loket: TANGSEL" },
+          { waktu: "2025-08-25 14:07:56", keterangan: "Tiba di Hub: JAKSEL" },
+          { waktu: "2025-08-26 08:44:01", keterangan: "Diteruskan ke Kantor Tujuan" }
+        ]
+      }
+    ],
 
     selectedPaket: '',
 
     form: {
-
       nim: '',
       nama: '',
       ekspedisi: ''
-
     }
-
   },
 
   computed: {
